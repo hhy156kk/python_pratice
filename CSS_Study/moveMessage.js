5179212d6333603f5e7986e33c3c4ed5e4e9f021
hhy156kk <hhy156kk@163.com>
2016-07-10 17:54:31 +0800

    css study

---
+++ CSS_Study/moveMessage.js
@@ -0,0 +1,7 @@
+function moveMessage(){
+	if(!document.getElementById) return false;
+	if(!document.getElementById("message")) return false;
+	var elem = document.getElementById("message");
+	elem.style.left = "200px";
+}
+addLoadEvent(moveMessage);
