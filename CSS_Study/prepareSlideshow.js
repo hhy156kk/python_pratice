779a9d003876793f75ad91f04f92d03dec014cd0
hhy156kk <hhy156kk@163.com>
2016-07-19 01:44:19 +0800

    jss_study

--- CSS_Study/prepareSlideshow.js
+++ CSS_Study/prepareSlideshow.js
@@ -1,17 +1,18 @@
 function prepareSlideshow(){
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
-
 	if(!document.getElementById("linklist")) return false;
-	if(!document.getElementById("preview")) return false;
 
-	var preview = document.getElementById("preview");
-	preview.style.position = "absolute";
-	preview.style.left = "0px";
-	preview.style.top = "0px";
-
+	var slideshow = document.createElement("div");
+	slideshow.setAttribute("id",slideshow);
+	var preview = document.createElement("img");
+	preview.setAttribute("src","images/BranStark.jpg");
+	preview.setAttribute("alt","building blocks of web design");
+	preview.setAttribute("id","preview");
+	slideshow.appendChild(preview);
 	var list = document.getElementById("linklist");
-	var links = list.getElementsByTagName("a");
+	insertAfter(slideshow,list);
+
 
 	links[0].onmouseover = function(){
 		moveElement("preview",-100,0,10);
