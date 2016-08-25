3954f05b5b2a51d576b64667876ac679565b1aea
hhy156kk <hhy156kk@163.com>
2016-08-26 00:21:15 +0800

    task18 1.0

--- task18/task.js
+++ task18/task.js
@@ -1,31 +1,31 @@
-function addEvent(element, event, listener) {
-    if (element.addEventListener) {
-        element.addEventListener(event, listener, false);
-    }
-    else if (element.attachEvent) {
-        element.attachEvent("on" + event, listener);
-    }
-    else {
-        element["on" + event] = listener;
-    }
-}
-
-function FleftIn(input){
+(function(){
 	
-		var divElement = document.createElement("div");
-		var text = document.createTextNode(input.toString());
-		divElement.appendChild(text);
-		var container = document.getElementById("container");
-		var target = container.getElementsByTagName("div")[0];
-		container.insertBefore(divElement,target);
-}
-window.onload = function(){
-	var leftIn = document.getElementById("left-in");
-	addEvent(leftIn,"click",function(){
+	var buttonLeftIn = document.getElementById("left-in");
+	buttonLeftIn.addEventListener("click",function(){
 		var input = document.getElementById("input-queue").value;
 		if(/^[0-9]+$/.test(input)){
-			FleftIn(input)
-		}
-	});
+			var divElement = document.createElement("div");
+			var text = document.createTextNode(input.toString());
+			divElement.appendChild(text);
 
-}
+			var container = document.getElementById("container");
+			var target = container.getElementsByTagName("div")[0];
+			container.insertBefore(divElement,target);
+		}
+
+	},false);
+
+	var buttonRightIn = document.getElementById("right-in");
+	buttonRightIn.addEventListener("click",function(){
+		var input = document.getElementById("input-queue").value;
+		if(/^[0-9]+$/.test(input)){
+			var divElement = document.createElement("div");
+			var text = document.createTextNode(input.toString());
+			divElement.appendChild(text);
+
+			var container = document.getElementById("container");
+			container.appendChild(divElement);
+		}
+
+	},false);
+})();
