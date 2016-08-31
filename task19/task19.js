9f740301fc5310a27452a43ff3fa446738f16c5f
hhy156kk <hhy156kk@163.com>
2016-09-01 00:44:54 +0800

    task19

--- task19/task19.js
+++ task19/task19.js
@@ -29,6 +29,32 @@ var Queue = {
 		container.innerHTML = str;
 	}
 
+	Qsort:function(){
+		quickSort(this.list);
+	}
+
+	quickSort:function(arr){
+		if (arr.length <= 1){
+			return arr;
+		}
+
+		var index = Math.floor(arr.length / 2);
+		var value = arr.splice(index,1)[0];
+		var left = [];
+		var right = [];
+
+		for(var i=0;i<arr.length;i++){
+			if(arr[i] <= value){
+				left.push(arr[i]);
+			}else{
+				right.push(arr[i]);
+			}
+		}
+
+		return quickSort(left).concat([value],quickSort(right));
+
+	}
+
 
 };
 
@@ -86,6 +112,12 @@ var Queue = {
 		}
 
 		Queue.rightPop();
+		Queue.paint();
+	},false);
+
+	var sortButton = document.getElementById("sort");
+	sortButton.addEventListener("click",function(){
+		Queue.Qsort();
 		Queue.paint();
 	},false);
 
